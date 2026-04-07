f0d278103a7d3ddaba4a3d666b631b3d79a8f4c6
Wafee Rahman <112278352+WafeeRahman@users.noreply.github.com>
2026-04-06 19:50:44 -0700

    Add: reorganize-string - submission-0

---
+++ Data Structures & Algorithms/reorganize-string/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        countS = defaultdict(int)
+
+        for char in s:
+            countS[char] += 1
+        charHeap = []
+        for key in countS:
+            heapq.heappush(charHeap, [-countS[key], key])
+
+        addQ = deque([])
+        res = ""
+        insertionPoint = 0
+        prev = None
+        while charHeap:
+            newChar = heapq.heappop(charHeap)
+            newChar[0] += 1
+            
+            if prev:
+                heapq.heappush(charHeap, prev)
+                prev = None
+            
+            
+            prev=newChar if newChar[0] < 0 else None
+            res+=newChar[1]
+            
+
+        
+
+        return res if len(res) == len(s) else ''
+
+
+ 
+
